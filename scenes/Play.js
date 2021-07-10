0789a98f69f341edc4506a3d03949a3c4fc6d778
TheUnaverageJoe <jobrcart@ucsc.edu>
2021-07-09 23:58:30 -0700

    Beginning of Menu Buttons

--- scenes/Play.js
+++ scenes/Play.js
@@ -4,7 +4,9 @@ class Play extends Phaser.Scene {
     }
     
     preload() {
-
+        this.load.image('bg', './assets/back.png');
+        this.load.image('menu', './assets/menu.png');
+        this.load.image('closeMenu', './assets/dropMenu.png');
     }
 
     create() {
@@ -23,11 +25,44 @@ class Play extends Phaser.Scene {
         }
         //this.bgText = this.add.text(game.config.width/2, game.config.height/2, "Playing", playTextConfig).setOrigin(.5,.5);
 
+        this.add.image(0, 0, 'bg').setOrigin(0,0);
+
         //initiallization section
+        const TOP = 3;
+        const MIDDLE = 2;
+        const BOTTOM = 1;
         this.scoreText = this.add.text(game.config.width/4 * 2, borderPadding, "Life Essence: ", playTextConfig).setOrigin(0,0);
         this.pointer = game.input.activePointer;
         this.score = 0;
         this.canPress = false;
+        this.displayMenu = false;
+        var rect;
+
+        this.menuButton = this.add.image(game.config.width - 41, game.config.height - 25, 'menu').setOrigin(0,0).setInteractive();
+        this.closeMenuButton = this.add.image(game.config.width - 37, game.config.height - 100, 'closeMenu').setOrigin(0,0);
+        this.closeMenuButton.depth = TOP;
+        this.closeMenuButton.alpha = 0;
+
+
+        this.menuButton.on('pointerdown', function(){
+            this.displayMenu = true;
+            rect = this.add.rectangle(0, game.config.height-100, game.config.width, 100, 0x2A7EFB, 1).setOrigin(0,0);
+            this.menuButton.disableInteractive();
+            this.closeMenuButton.alpha = 1;
+            this.closeMenuButton.setInteractive();
+            console.log("hi");
+
+        }, this);
+        this.closeMenuButton.on('pointerdown', function(){
+            this.displayMenu = false;
+            this.closeMenuButton.alpha = 0;
+            this.closeMenuButton.disableInteractive();
+            this.menuButton.setInteractive();
+
+            rect.destroy();
+            console.log("bye");
+        }, this);
+
     }
     update(){
         if(this.pointer.leftButtonDown() && this.canPress){
@@ -38,5 +73,10 @@ class Play extends Phaser.Scene {
             this.canPress = true;
         }
         this.scoreText.text = "Life Essence: " + this.score;
+    }
+
+    //method cannot be called for some reason
+    displayMenu(){
+        console.log("displayMenu function");
     }
 }
