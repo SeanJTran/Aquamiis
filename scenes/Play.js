0c680297f0ccb61bff8c6f8b57609d1bc359a1cd
TheUnaverageJoe <jobrcart@ucsc.edu>
2021-07-10 14:40:49 -0700

    Developing pod pull structure

--- scenes/Play.js
+++ scenes/Play.js
@@ -4,27 +4,32 @@ class Play extends Phaser.Scene {
     }
     
     preload() {
-        this.load.image('bg', './assets/back.png');
+        this.load.image('bg', './assets/bg.png');
         this.load.image('menu', './assets/menu.png');
         this.load.image('closeMenu', './assets/dropMenu.png');
         this.load.image('podsTab', './assets/podsTab.png');
+        this.load.image('creaturesTab', './assets/creaturesTab.png');
+        //Need to change path name for below images when assets are added
+        //this.load.image('ccc', './assets/.png');
+        //this.load.image('rcc', './assets/.png');
+        //this.load.image('lcc', './assets/.png');
+        
     }
 
     create() {
         let playTextConfig = {
             fontSize: '28px',
-            backgroundColor: '#111',
-            color: '#1EEBD6',
+            backgroundColor: null,
+            color: '#1A5BE6',
             padding: {
                 x: 5,
                 y: 5
             },
-            align: 'left',
+            align: 'right',
             stroke: '#1EEBD6',
             strokeThickness: 2,
             fixedWidth: game.config.width/2 - borderPadding
         }
-        //this.bgText = this.add.text(game.config.width/2, game.config.height/2, "Playing", playTextConfig).setOrigin(.5,.5);
 
         this.add.image(0, 0, 'bg').setOrigin(0,0);
 
@@ -38,8 +43,17 @@ class Play extends Phaser.Scene {
         this.canPress = false;
         this.displayMenu = false;
         var rect;
+        var collectedCreatures = [];
+        var commonCreatures = ['Imi', 'Oop', 'Kle', 'Amo'];
+        var rareCreatures = ['Kran', 'Jept', 'Rano', 'Welm'];
+        var legendaryCreatures = ['Swamp', 'River', 'Grass', 'Lives'];
 
+        //interactable menu setup
         this.menuButton = this.add.image(game.config.width - 37, game.config.height - 23, 'menu').setOrigin(0,0).setInteractive();
+        this.menuButtonText = this.add.text(game.config.width - 37 - 60, game.config.height - 20, "Menu", {
+            fontSize: '24px',
+            color: '#1A5BE6',
+        }).setOrigin(0,0);
 
         this.closeMenuButton = this.add.image(game.config.width - 41, game.config.height - 150, 'closeMenu').setOrigin(0,0);
         this.closeMenuButton.depth = TOP;
@@ -47,6 +61,9 @@ class Play extends Phaser.Scene {
 
         this.pods = this.add.image(0/*(112)*/, game.config.height - 150 - 33, 'podsTab').setOrigin(0,0);
         this.pods.alpha = 0;
+
+        this.creatures = this.add.image(112, game.config.height - 150 - 33, 'creaturesTab').setOrigin(0,0);
+        this.creatures.alpha = 0;
 
         this.buy1 = this.add.image(game.config.width/4 * 1, game.config.height - borderUISize - 50, 'ccc');
         this.buy1.alpha = 0;
@@ -56,7 +73,7 @@ class Play extends Phaser.Scene {
         this.buy2.alpha = 0;
         this.buy2.depth = MIDDLE;
 
-        this.buy3 = this.add.image(game.config.width/4 * 3, game.config.height - borderUISize - 50, 'ucc');
+        this.buy3 = this.add.image(game.config.width/4 * 3, game.config.height - borderUISize - 50, 'lcc');
         this.buy3.alpha = 0;
         this.buy3.depth = MIDDLE;
 
@@ -64,18 +81,22 @@ class Play extends Phaser.Scene {
             if(this.score >= 10){
                 this.score -= 10;
             }
+            this.pullCreature('C');
         }, this);
         this.buy2.on('pointerdown', function(){
             if(this.score >= 100){
                 this.score -= 100;
             }
+            this.pullCreature('R');
         }, this);
         this.buy3.on('pointerdown', function(){
             if(this.score >= 1000){
                 this.score -= 1000;
             }
+            this.pullCreature('L');
         }, this);
 
+        //bring up menu
         this.menuButton.on('pointerdown', function(){
             this.displayMenu = true;
             rect = this.add.rectangle(0, game.config.height-150, game.config.width, 150, 0x2A7EFB, 1).setOrigin(0,0);
@@ -84,9 +105,14 @@ class Play extends Phaser.Scene {
             this.closeMenuButton.alpha = 1;
             this.closeMenuButton.setInteractive();
 
+            //make tabs interactive
             this.pods.alpha = 1;
             this.pods.setInteractive();
 
+            this.creatures.alpha = 1;
+            this.creatures.setInteractive();
+
+            //display creatures content by default
             this.buy1.alpha = 1;
             this.buy1.setInteractive();
             this.buy2.alpha = 1;
@@ -94,21 +120,28 @@ class Play extends Phaser.Scene {
             this.buy3.alpha = 1;
             this.buy3.setInteractive();
             console.log("hi");
-
         }, this);
 
+
+        //close menu button
         this.closeMenuButton.on('pointerdown', function(){
             this.displayMenu = false;
             this.closeMenuButton.alpha = 0;
             this.closeMenuButton.disableInteractive();
 
+            //hide tabs
             this.pods.alpha = 0;
             this.pods.disableInteractive();
 
+            this.creatures.alpha = 0;
+            this.creatures.disableInteractive();
+
             this.menuButton.setInteractive();
 
+            //hide background of menu
             rect.destroy();
 
+            //hide pods tab content
             this.buy1.alpha = 0;
             this.buy1.disableInteractive();
             this.buy2.alpha = 0;
@@ -116,10 +149,16 @@ class Play extends Phaser.Scene {
             this.buy3.alpha = 0;
             this.buy3.disableInteractive();
 
-            console.log("bye");
+            //hide creatures tab content
+            //---Here----
         }, this);
 
+        //pressed pods tab
         this.pods.on('pointerdown', function(){
+            //disable creatures tab display
+            //----here----
+
+            //display perchasable containers for creatures
             console.log("pods clicked");
             this.buy1.alpha = 1;
             this.buy1.setInteractive();
@@ -127,7 +166,20 @@ class Play extends Phaser.Scene {
             this.buy2.setInteractive();
             this.buy3.alpha = 1;
             this.buy3.setInteractive();
-            //display perchasable containers for creatures
+        }, this);
+
+        //pressed creatures tab
+        this.creatures.on('pointerdown', function(){
+            //disable pods tab display
+            this.buy1.alpha = 0;
+            this.buy1.disableInteractive();
+            this.buy2.alpha = 0;
+            this.buy2.disableInteractive();
+            this.buy3.alpha = 0;
+            this.buy3.disableInteractive();
+            //code for codex display here
+
+            console.log("creatures clicked");
         }, this);
 
     }
@@ -143,11 +195,17 @@ class Play extends Phaser.Scene {
     }
 
     //method cannot be called for some reason
-    displayMenu(){
-        console.log("displayMenu function");
-    }
-
-    purchase(cost){
-        this.score -= cost;
+    pullCreature(letter){
+        switch(letter){
+            case 'C':
+                //do stuff
+                break;
+            case 'R':
+                //do stuff
+                break;
+            case 'L':
+                //do stuff
+                break;
+        }
     }
 }
